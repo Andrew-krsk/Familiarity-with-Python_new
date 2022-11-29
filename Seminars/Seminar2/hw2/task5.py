d846425f9bc2ee9e24986bc957b681eaa26aa7fe
Andrew <>
2022-11-29 14:43:02 +0700

    Python Seminar2 hw task5 29.11.22

--- Seminars/Seminar2/hw2/task5.py
+++ Seminars/Seminar2/hw2/task5.py
@@ -1,1 +1,22 @@
 # Реализуйте алгоритм перемешивания списка.
+
+import random
+from random import randint
+
+list = []
+for i in range(10):
+    list.append(randint(-10, 11))
+
+print(list)
+
+for i in range(len(list)-1):
+    rand_index = randint(0, len(list)-1)
+    temp = list[i]
+    list[i] = list[rand_index]
+    list[rand_index] = temp
+
+print(list)
+
+
+# random.shuffle(list)  
+# print(list)
