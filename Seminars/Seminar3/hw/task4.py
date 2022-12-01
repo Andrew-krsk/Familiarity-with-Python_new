4ce01f2eaf5e68d78b5ddf49f428942bcff0ae48
Andrew <>
2022-12-01 13:07:31 +0700

    Python Seminar3 hw task4 1.12.22

--- Seminars/Seminar3/hw/task4.py
+++ Seminars/Seminar3/hw/task4.py
@@ -5,3 +5,23 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+def convert_to_binary(n) -> list[str]:
+    list = []
+    while n > 1:
+        if n%2 == 0:
+            list.insert(0, '0')
+        else:
+            list.insert(0, '1')
+        n //= 2
+    list.insert(0, '1')
+    return list
+
+
+n = int(input('Введите значение десятичного числа: '))
+list = convert_to_binary(n)
+print(f'{n} -> ', end='')
+
+for i in range(len(list)):
+    print(list[i], end='')
+
